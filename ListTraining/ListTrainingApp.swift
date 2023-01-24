a7548e27e280689cc7761e91205db12aed087b89
l1terally <literallyuriy@gmail.com>
2023-01-24 14:49:58 +0800

    Initial Commit

---
+++ ListTraining/ListTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTrainingApp.swift
+//  ListTraining
+//
+//  Created by Юрий  Лизунов on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ListTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
